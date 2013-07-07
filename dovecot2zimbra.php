7bd9090acd13943cbcf40ec228b5217f471e48be
Ruben Lacasa <ruben@ensenalia.com>
2013-07-07 21:38:54 +0200

    Some modf

--- dovecot2zimbra.php
+++ dovecot2zimbra.php
@@ -17,23 +17,30 @@ $masterUser1 = 'masterUserOrigin'; // Master User Server Origin
 $masterPwd1 = 'masterUserOriginPwd'; // Master User Server Origin Password
 $masterUser2 = 'masterUserDest'; // Master User Server Dest
 $masterPwd2 = 'masterUserDestPwd'; // Master User Server Dest Password
+$domain = false; // retrive the actual domain
 if (is_file($usersFile)) {
     $file = file($usersFile);
     foreach ($file as $line) {
         $data = explode(":", $line);
         $userMail = trim($data[0]);
         $userPass = trim($data[1]);
+        //Create Domain if not exists
+        $mailParts = explode("@", $userMail);
+        if (trim($mailParts[1]) != $domain) {
+            $domain = trim($mailParts[1]);
+            exec("zmprov createDomain ".$domain);
+        }
         echo "echo 'Syncing User " . $userMail . " from " . $hostOrigin . " to " . $hostDest . "'" . PHP_EOL;
         // User creation
         exec("zmprov ca " . $userMail . " temppasswordAAA");
         // Not work width CRAM-MD5 password
         if (preg_match('/CRAM-MD5/', $userPass)) {
-          $password = "'" . $userPass . "'";
+            $password = "'" . $userPass . "'";
         } else {
-          $password = "'{crypt}" . $userPass . "'";    
+            $password = "'{crypt}" . $userPass . "'";    
         }
         exec("zmprov ma " . $userPass . " userPassword " . $password);
         // Mailbox migration  
-        exec("imapsync --nosyncacls --syncinternaldates \--host1   " . $hostOrigin . " --user1 " . $userMail . " --authuser1 " . $masterUser . " --password1 " . $masterPwd . " \--host2 " . $hostDest . " --user2 " . $userMail . " --authuser2 " . $masterUser2 . " --password2 " . $masterPwd2 . " --ssl2");      
+        exec("imapsync --nosyncacls --syncinternaldates \--host1   " . $hostOrigin . " --user1 " . $userMail . " --authuser1 " . $masterUser . " --password1 " . $masterPwd . " \--host2 " . $hostDest . " --user2 " . $userMail . " --authuser2 " . $masterUser2 . " --password2 " . $masterPwd2 . " --ssl2");  
     }
 }
